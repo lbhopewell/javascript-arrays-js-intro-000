666b90485cfcb60ed60233b659c64f8f6728a99c
Bruce Hopewell <lbhopewell@outlook.com>
2018-09-12 00:44:46 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,47 @@
+var chocolateBars = ["snicker", "hundred grand", "kitkat", "skittles"];
+
+function addElementToBeginningOfArray(array, element){
+  let newArray = [element, ...array];
+  return array;
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element){
+  array.unshift(element)
+  return array;
+}
+
+function addElementToEndOfArray(array, element){
+  var newArray = array.concat(element)
+  return array;
+}
+
+function destructivelyAddElementToEndOfArray(array,element){
+  array.push(element)
+  return array
+  
+}
+
+function accessElementInArray(array, index){
+return array[index]
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array){
+  array.shift()
+  return array;
+}
+
+function removeElementFromBeginningOfArray(array){
+  newArray = array.slice(1)
+  return array;
+}
+
+function destructivelyRemoveElementFromEndOfArray(array){
+  array.pop()
+  return array;
+  
+}
+
+function removeElementFromEndofArray(array){
+  newArray = array.slice(0, array.length - 1)
+  return array
+}
